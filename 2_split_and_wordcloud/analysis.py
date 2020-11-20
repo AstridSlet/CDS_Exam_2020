53a3a4e9c9e9fe7ef54d06309122a51aefe900b2
Emil_Jessen <emil.tj@hotmail.com>
2020-11-20 13:20:44 +0100

    with '#' will be ignored, and an empty message aborts the commit.

--- 2_split_and_wordcloud/analysis.py
+++ 2_split_and_wordcloud/analysis.py
@@ -29,7 +29,7 @@ emoji_pattern = re.compile("["
         u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
         u"\U0001F1F2-\U0001F1F4"  # Macau flag
         u"\U0001F1E6-\U0001F1FF"  # flags
-        u"\U0001F600-\U0001F64F"
+        u"\U0001F600-\U0001F64F"  # below is specific emojis that weren't included in the above
         u"\U00002702-\U000027B0"
         u"\U000024C2-\U0001F251"
         u"\U0001f926-\U0001f937"
